6fed530d81d4408a9aa4b0f4eb0d8799de387558
vebradev <justinas@vebra.dev>
2019-05-09 14:42:43 +0300

    Converted my constructors to Classes.

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -8,88 +8,47 @@ Prototype Refactor
 
 */
 
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
-
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
-
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
-
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
-
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
-
-/*
- * Inheritance chain: GameObject -> CharacterStats -> Humanoid
- * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
- * Instances of CharacterStats should have all of the same properties as GameObject.
- */
-
-// Test you work by un-commenting these 3 objects and the list of console logs below:
-
 // GameObject
 
-function GameObject(argument) {
-  this.createdAt = argument.createdAt;
-  this.name = argument.name;
-  this.dimensions = argument.dimensions;
-}
+class GameObject {
+  constructor(argument) {
+    this.createdAt = argument.createdAt;
+    this.name = argument.name;
+    this.dimensions = argument.dimensions;
+  }
 
-GameObject.prototype.destroy = function() {
-  return `${this.name} was removed from the game.`;
-};
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
 
 // CharacterStats
 
-function CharacterStats(argument) {
-  GameObject.call(this, argument);
-  this.healthPoints = argument.healthPoints;
+class CharacterStats extends GameObject {
+  constructor(argument) {
+    super(argument);
+    this.healthPoints = argument.healthPoints;
+  }
+
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
 }
-
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`;
-};
 
 // Humanoid
 
-function Humanoid(argument) {
-  CharacterStats.call(this, argument);
-  this.team = argument.team;
-  this.weapons = argument.weapons;
-  this.language = argument.language;
+class Humanoid extends CharacterStats {
+  constructor(argument) {
+    super(argument);
+    this.team = argument.team;
+    this.weapons = argument.weapons;
+    this.language = argument.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
 }
-
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}`;
-};
 
 const mage = new Humanoid({
   createdAt: new Date(),
@@ -143,8 +102,3 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
-// Stretch task:
-// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
-// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-// * Create two new objects, one a villain and one a hero and fight it out with methods!
